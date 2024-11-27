ec0a3af049ef577e23a576c1b7f45798af911f63
Youta <0x59.xxx@gmail.com>
2024-11-27 12:06:58 +0900

    Delete: delete rows related to PWA in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
-  dest: 'public', // PWA関連ファイルの出力先
-  disable: process.env.NODE_ENV === 'development', // 開発環境では無効化
+const nextConfig = {
   images: {
-    domains: ['upload.wikimedia.org'], // 外部ドメインを許可
+      domains: ['upload.wikimedia.org'], // 外部ドメインを許可
   },
-  reactStrictMode: true, // 他の設定
-});
+};
 
 export default nextConfig;
